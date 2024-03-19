34cd5765c5659336a5aacd654e5287199b525dae
KaosStorm <145504760+KaosStorm@users.noreply.github.com>
2024-03-19 15:27:51 -0700

    th
    th

--- BasicGamePlayCastilloP8/Assets/Scripts/DestroyOutOfBounds.cs
+++ BasicGamePlayCastilloP8/Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 30;
+    private float topBound = 35;
     private float lowerBound = -10;
 
     // Start is called before the first frame update
@@ -17,10 +17,11 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z < topBound)
+        if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z > lowerBound)
+        } 
+        else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }
