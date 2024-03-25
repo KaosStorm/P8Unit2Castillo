dbd60881ff805a1d42e9f3d9c05866af5d80eba1
KaosStorm <145504760+KaosStorm@users.noreply.github.com>
2024-03-25 12:22:40 -0700

    fsdh
    shdf

--- BasicGamePlayCastilloP8/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayCastilloP8/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    private float zTopRange = 15f;
+    private float zLowerRange = 0;
 
     public GameObject projectilePrefab;
 
@@ -28,9 +31,20 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (transform.position.z > zTopRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zTopRange);
+        }
+        if (transform.position.z < -zLowerRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zLowerRange);
+        }
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
